2c9367e92bb490fc9af0b3cc6e3dc6c1e89d63b7
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-04-16 16:49:47 -0300

    Cadastro de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express'
-import User from './app/models/User'
+
+import UserController from './app/controllers/UserController'
 
 const routes = new Router()
 
-routes.get('/teste', async (req, res) => {
-    const user = await User.create({
-        name: 'Leandro',
-        email: 'leandrolima@leandro.com',
-        password_hash: '123123'
-    })
-
-    return res.json(user)
-})
+routes.post('/users', UserController.store)
 
 export default routes
